e50df35c7d40753d3ca07c42dc2dc82ffb04ac60
leo.jaimes <leo777jaimes@gmail.com>
2022-08-01 20:59:58 -0500

    upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { ShopLayout } from '../components/layouts/ShopLayout';
 
 
 const Home: NextPage = () => {
   return (
-    <div  >
-      Teslo - shop
-    </div>
+    <ShopLayout title={'Home - Teslo Shop'} pageDescription={'Found the best products from Teslo Here!!'}     >
+      <Typography
+        component={'h1'}
+        variant='h1'
+      >
+        Shop
+      </Typography>
+
+      <Typography
+        component={'h2'}
+        variant='h2'
+        sx={{
+          marginBottom: '1px'
+        }}
+      >
+        All Products
+      </Typography>
+    </ShopLayout>
   )
 }
 
