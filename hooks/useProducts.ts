fc392dc1b238a8fc7eb6ad1f1e5e6f2291bfcb27
leo.jaimes <leo777jaimes@gmail.com>
2022-08-08 17:37:59 -0500

    upload

--- hooks/useProducts.ts
+++ hooks/useProducts.ts
@@ -17,3 +17,4 @@ export const useProducts = (url: string, config: SWRConfiguration = {}) => {
 
     }
 }
+    
--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,128 +1,153 @@
 import { Box, Button, Chip, Grid, Typography } from '@mui/material';
-import React from 'react'
+import { GetServerSidePropsContext, NextPage } from 'next';
+import { useRouter } from 'next/router';
+import React, { FC } from 'react'
 import { ShopLayout } from '../../components/layouts/ShopLayout';
 import { ProductSlideShow, SizeSelector } from '../../components/products';
-import { ItemCounter } from '../../components/ui';
-import { initialData } from '../../database/products';
+import { FullScreenLoading, ItemCounter } from '../../components/ui';
+import { IProduct } from '../../interfaces';
+//import { GetServerSideProps } from 'next'
+//import { getProductBySlug } from '../../database/dbProducts';
+import { dbProducts, } from '../../database';
 
-const product = initialData.products[0]
+import { GetStaticProps } from 'next'
+import { GetStaticPaths } from 'next'
 
-const ProductPage = () => {
+//const product = initialData.products[0]
+
+
+interface Props {
+    product: IProduct
+
+}
+
+const ProductPage: NextPage<Props> = ({ product }) => {
+
+
     return (
+
+
         <ShopLayout title={`${product.title}`} pageDescription={`this is a ${product.title}`}
         >
-            <Grid
-                container
-                spacing={3}
-            >
 
-                <Grid item xs={12} sm={7}>
+            {
+                !product
+                    ? <FullScreenLoading />
+                    :
 
-                    <ProductSlideShow images={product.images} />
-
-
-                </Grid>
-
-                <Grid item xs={12} sm={5}>
-
-                    <Box
-                        display='flex'
-                        flexDirection='column'
-
+                    <Grid
+                        container
+                        spacing={3}
                     >
 
-                        {/** Tittles */}
+                        <Grid item xs={12} sm={7}>
 
-                        <Typography
-                            variant='h1'
-                            component='h1'
-                        >
-                            {product.title}
-                        </Typography>
-
-                        <Typography
-                            variant='subtitle1'
-                            component='h2'
-                        >
-                            ${product.price}
-                        </Typography>
+                            <ProductSlideShow images={product.images} />
 
 
+                        </Grid>
 
-                        <Box
-                            sx={{
-                                margin: '2px 2px'
-                            }}
-                        >
-                            <Typography
-                                variant='subtitle2'
-                                component='h2'
+                        <Grid item xs={12} sm={5}>
+
+                            <Box
+                                display='flex'
+                                flexDirection='column'
+
                             >
-                                Quantity ${product.price}
-                            </Typography>
-                            <ItemCounter />
-                            <SizeSelector
-                                sizes={product.sizes}
-                                selectedSize={product.sizes[0]}
-                            />
 
-                        </Box>
+                                {/** Tittles */}
 
+                                <Typography
+                                    variant='h1'
+                                    component='h1'
+                                >
+                                    {product.title}
+                                </Typography>
 
-                        {/**Add To cart */}
-                        <Button
-                            color='secondary'
-                            className='circular-btn'
-
-                        >   Add to cart
-
-                        </Button>
-
-                        {
-                            false && (
-                                <Chip
-                                    label='Not Available'
-                                    color='error'
-                                    variant='outlined'
-
-                                />
-                            )
-                        }
-
-
-                        {/** description */}
-
-                        <Box
-                            sx={{
-                                marginTop: '3px'
-                            }}
-
-                        >
-
-                            <Typography
-
-                                variant='subtitle2'
-                            >
-                                Description:
-                            </Typography>
-
-
-                            <Typography
-
-                                variant='body2'
-                            >
-                                {product.description}
-                            </Typography>
-                        </Box>
+                                <Typography
+                                    variant='subtitle1'
+                                    component='h2'
+                                >
+                                    ${product.price}
+                                </Typography>
 
 
 
-                    </Box>
-                </Grid>
+                                <Box
+                                    sx={{
+                                        margin: '2px 2px'
+                                    }}
+                                >
+                                    <Typography
+                                        variant='subtitle2'
+                                        component='h2'
+                                    >
+                                        Quantity ${product.price}
+                                    </Typography>
+                                    <ItemCounter />
+                                    <SizeSelector
+                                        sizes={product.sizes}
+                                        selectedSize={product.sizes[0]}
+                                    />
+
+                                </Box>
+
+
+                                {/**Add To cart */}
+                                <Button
+                                    color='secondary'
+                                    className='circular-btn'
+
+                                >   Add to cart
+
+                                </Button>
+
+                                {
+                                    false && (
+                                        <Chip
+                                            label='Not Available'
+                                            color='error'
+                                            variant='outlined'
+
+                                        />
+                                    )
+                                }
+
+
+                                {/** description */}
+
+                                <Box
+                                    sx={{
+                                        marginTop: '3px'
+                                    }}
+
+                                >
+
+                                    <Typography
+
+                                        variant='subtitle2'
+                                    >
+                                        Description:
+                                    </Typography>
+
+
+                                    <Typography
+
+                                        variant='body2'
+                                    >
+                                        {product.description}
+                                    </Typography>
+                                </Box>
 
 
 
-            </Grid>
+                            </Box>
+                        </Grid>
+
+
+
+                    </Grid>
+            }
 
         </ShopLayout>
     )
@@ -130,5 +155,85 @@ const ProductPage = () => {
 
 
 
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+
+
+/*export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+    const { slug = '' } = ctx.params as { slug: string };
+
+    const product = await getProductBySlug(slug) // your fetch function here 
+
+
+    if (!product) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
+    return {
+        props: {
+            product
+        }
+    }
+}*/
+
+
+// You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+    //const { data } = await  // your fetch function here 
+
+
+    const productSlugs = await dbProducts.getAllProductSlugs();
+
+
+    return {
+        paths: productSlugs.map(({ slug }) => ({
+            params: {
+                slug
+            }
+        })),
+        fallback: 'blocking'
+    }
+
+
+}
+
+
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    //const { data } = await  // your fetch function here 
+    const { slug = '' } = ctx.params as { slug: string };
+
+    const product = await dbProducts.getProductBySlug(slug)
+
+    if (!product) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
+
+    return {
+        props: {
+            product
+        },
+        revalidate: 60 * 60 * 24
+    }
+}
 
 export default ProductPage
