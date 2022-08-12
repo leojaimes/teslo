3d1f9ab97b195277ab801eb767087fffb6993742
leo.jaimes <leo777jaimes@gmail.com>
2022-08-12 12:53:03 -0500

    upload

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,42 @@
 import { NextFetchEvent, NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { jwt } from './utils';
+import { jwt } from './utils'
+ 
+
 
 export async function middleware(request: NextRequest, event: NextFetchEvent) {
- 
-    const response = new NextResponse()
-  
+
+
+
     const loginUrl = new URL('/auth/login', request.url)
     loginUrl.searchParams.set('p', request.nextUrl.pathname)
 
 
-    try{
+    const token = request.cookies.get('token')
 
-    }catch(error) {
-        
+    if (!token) {
+        return NextResponse.redirect(loginUrl)
     }
-    return NextResponse.redirect(loginUrl)
+
+    // jwt.isValidToken esta usando una libreria que no es aceptada en el edge algo no usarla  
+    return NextResponse.next()
+
+   
+
+    // try {
+    //     await axios.post('/route')
+    // } catch (error) {
+
+    // }
+    // return NextResponse.redirect(loginUrl)
 
 
- 
- 
-  }
+
+
+}
 
 
 
-  export const config = {
+export const config = {
     matcher: ['/checkout/:path*', '/dashboard/:path*'],
-  }
+}
