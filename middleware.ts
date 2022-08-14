729c2ff2c42060bb9bc21362d198c65851ae20f5
leo.jaimes <leo777jaimes@gmail.com>
2022-08-14 13:38:08 -0500

    upload

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,49 @@
 import { NextFetchEvent, NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { jwt } from './utils'
- 
-
-
-export async function middleware(request: NextRequest, event: NextFetchEvent) {
+import { getToken } from 'next-auth/jwt';
 
 
 
-    const loginUrl = new URL('/auth/login', request.url)
-    loginUrl.searchParams.set('p', request.nextUrl.pathname)
+export async function middleware(req: NextRequest | any, ev: NextFetchEvent) {
 
 
-    const token = request.cookies.get('token')
 
-    console.log(token)
-    if (!token) {
-        return NextResponse.redirect(loginUrl)
-    }
+    // const loginUrl = new URL('/auth/login', request.url)
+    // loginUrl.searchParams.set('p', request.nextUrl.pathname)
+
+
+    // const token = request.cookies.get('token')
+
+    // console.log(token)
+    // if (!token) {
+    //     return NextResponse.redirect(loginUrl)
+    // }
 
     // jwt.isValidToken esta usando una libreria que no es aceptada en el edge algo no usarla  
-    return NextResponse.next()
 
-   
+    const received = { req, secret: process.env.NEXTAUTH_SECRET }
+    console.log(received)
+
+    const session = await getToken(received);
+
+    console.log(`Session from middleware`)
+    console.log({ session });
+
+
+    const loginUrl = new URL('/auth/login', req.url)
+    loginUrl.searchParams.set('p', req.nextUrl.pathname)
+
+
+    if (!session) {
+
+        //return NextResponse.redirect(loginUrl)
+    }
+
+    return NextResponse.next();
+
+
+
 
     // try {
     //     await axios.post('/route')
