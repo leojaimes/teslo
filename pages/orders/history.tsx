2eb61cdf94ec8afd31f7aaf153eb277289a17ccf
leo.jaimes <leo777jaimes@gmail.com>
2022-08-15 11:51:59 -0500

    upload

--- pages/orders/history.tsx
+++ pages/orders/history.tsx
@@ -1,55 +1,49 @@
 import NextLink from 'next/link'
+import { GetServerSideProps, NextPage } from 'next'
+import { getSession } from 'next-auth/react';
+
 
 import { Chip, Grid, Typography, Link } from '@mui/material';
 import { DataGrid, GridColDef, GridValueGetterParams, } from '@mui/x-data-grid';
 import { ShopLayout } from '../../components/layouts/ShopLayout';
 
+
+import { dbOrders } from '../../database';
+import { IOrder } from '../../interfaces';
+
+
 const columns: GridColDef[] = [
-    {
-        field: 'id',
-        headerName: 'ID',
-        width: 100
-
-    },
-    {
-        field: 'fullname',
-        headerName: 'Full Name',
-        width: 300
-
-    },
+    { field: 'id', headerName: 'ID', width: 100 },
+    { field: 'fullname', headerName: 'Nombre Completo', width: 300 },
 
     {
         field: 'paid',
-        headerName: 'Paid',
+        headerName: 'Pagada',
+        description: 'Muestra información si está pagada la orden o no',
         width: 200,
-        description: 'Shows if order is paid',
         renderCell: (params: GridValueGetterParams) => {
-            return (params.row.paid
-                ? <Chip color='success' label='Paid' variant='outlined' />
-                : <Chip color='error' label='UnPaid' variant='outlined' />)
+            return (
+                params.row.paid
+                    ? <Chip color="success" label="Pagada" variant='outlined' />
+                    : <Chip color="error" label="No pagada" variant='outlined' />
+            )
         }
-
-
     },
-
     {
-        field: 'order',
-        headerName: 'Order',
+        field: 'orden',
+        headerName: 'Ver orden',
         width: 200,
-        description: 'Go to Order',
         sortable: false,
         renderCell: (params: GridValueGetterParams) => {
             return (
-                <NextLink href={`/orders/${params.row.id}`} passHref>
-                    <Link  underline='always' >Order</Link>
+                <NextLink href={`/orders/${params.row.orderId}`} passHref>
+                    <Link underline='always'>
+                        Ver orden
+                    </Link>
                 </NextLink>
             )
         }
-
-
-    },
-
-
+    }
 ];
 
 
@@ -64,23 +58,65 @@ const rows = [
 
 
 
-const HistoryPage = () => {
+interface Props {
+    orders: IOrder[]
+}
+
+
+const HistoryPage: NextPage<Props> = ({ orders }) => {
+
+    const rows = orders.map((order, idx) => ({
+        id: idx + 1,
+        paid: order.isPaid,
+        fullname: `${order.shippingAddress.firstName} ${order.shippingAddress.lastName}`,
+        orderId: order._id
+    }))
+
+
     return (
         <ShopLayout title={'Order History - Teslo'} pageDescription={'Order History - Teslo'}>
 
             <Typography variant='h1' component='h1'>Order History</Typography>
-            <Grid container>
+            <Grid container className='fadeIn'>
                 <Grid item xs={12} sx={{ height: 650, width: '100%' }}>
                     <DataGrid
-                        columns={columns}
                         rows={rows}
+                        columns={columns}
                         pageSize={10}
                         rowsPerPageOptions={[10]}
                     />
+
                 </Grid>
             </Grid>
         </ShopLayout>
     )
 }
+
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+
+    const session: any = await getSession({ req });
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth/login?p=/orders/history',
+                permanent: false,
+            }
+        }
+    }
+
+    const orders = await dbOrders.getOrdersByUser(session.user._id);
+
+
+    return {
+        props: {
+            orders
+        }
+    }
+}
+
+
+
 
 export default HistoryPage
